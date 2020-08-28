c2993e595d8f037501701bbad5a99494626c8dfb
rhodso <rhodsoprivate@gmail.com>
2020-08-28 21:33:16 +0100

    Added source and header file generation
    Added ability to directly generate source and header files, as requested in #2

---
+++ testClass.h
@@ -0,0 +1,32 @@
+//Header file auto-generated using CPP-Getter-Setter-Generator
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+//Includes
+
+
+class testClass {
+	public:
+		//Constructor and Destructor
+		testClass();
+		~testClass();
+
+		//Getters and Setters
+		float getX();
+		float getY();
+		float getZ();
+
+		void setX( float _x );
+		void setY( float _y );
+		void setZ( float _z );
+
+		//Other methods
+
+	private:
+		//Variables
+		float x;
+		float y;
+		float z;
+
+};
+#endif
